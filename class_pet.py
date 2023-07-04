1be48a041653e4c8402265cb4573704684291036
Logie Mendiola <logiamendiola@iskolarngbayan.pup.edu.ph>
2023-07-04 20:19:06 +0800

    [P-9] Added Instructions for program flow

---
+++ class_pet.py
@@ -0,0 +1,7 @@
+#Name: MENDIOLA, LOGIE A | Year&Sec: BSCPE 1-5 | OBJECT ORIENTED PROGRAMMING | ASSIGNMENT #9
+
+print("======================================================================= INSTRUCTIONS ==============================================================================")
+print("                 Write a program that creates an object of the class and prompts the user to enter the name, type, and age of his or her pet.                      ")
+print("                                                  This data should be stored as the object’s attributes                                                            ")
+print("                               Use the object’s accessor methods to retrieve the pet’s name, type, and age and display this data on the screen.                    ")
+print("===================================================================================================================================================================")
