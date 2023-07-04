d680457103be03ab9dc492c27f0a25f4646c0dde
Logie Mendiola <logiamendiola@iskolarngbayan.pup.edu.ph>
2023-07-04 19:46:37 +0800

    [P-9] Assigned a class named Car

--- class_car.py
+++ class_car.py
@@ -6,3 +6,21 @@ print("                                    After each call to the accelerate met
 print("                                                           Then call the brake method five times                                                                   ")
 print("                                        After each call to the brake method get the current speed of the car and display it                                        ")
 print("===================================================================================================================================================================")
+
+class Car:
+    def __init__(self, year_model, make, speed = 0):
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = speed
+
+    #Car Acceleration
+    def accelerate(self):
+        self.__speed += 5
+
+    #Car Brake
+    def brake(self):
+        self.__speed -= 5
+
+    #getter method for speed    
+    def get_speed(self):
+        return self.__speed
