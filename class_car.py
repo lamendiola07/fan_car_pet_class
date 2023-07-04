f2263e2d08619ddda5d6af2bf68ec5fc0911e5e3
Logie Mendiola <logiamendiola@iskolarngbayan.pup.edu.ph>
2023-07-04 18:56:34 +0800

    [P-9] Included Instructions for program flow

---
+++ class_car.py
@@ -0,0 +1,8 @@
+#Name: MENDIOLA, LOGIE A | Year&Sec: BSCPE 1-5 | OBJECT ORIENTED PROGRAMMING | ASSIGNMENT #9
+
+print("======================================================================= INSTRUCTIONS ==============================================================================")                            
+print("                                     Design a program that creates a Car object then calls the accelerate method five times                                        ")
+print("                                    After each call to the accelerate method, get the current speed of the car and display it                                      ")
+print("                                                           Then call the brake method five times                                                                   ")
+print("                                        After each call to the brake method get the current speed of the car and display it                                        ")
+print("===================================================================================================================================================================")
